e222d41b318ad8526f5354eba1f337b78bca4f29
nazim1996 <nazimmd1996@gmail.com>
2022-09-27 21:21:00 +0530

    library added & code fixing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "../src/styles/App.css";
 import LoginContainer from "./modules/Login/Container/LoginContainer";
+import UplaodingDocContainer from "./modules/UploadingDoc/Container/UplaodingDocContainer";
 
 function App() {
   return <LoginContainer></LoginContainer>;
+  // return <UplaodingDocContainer></UplaodingDocContainer>;
 }
 
 export default App;
