4a9afd8133d15e7ed19e69130dbb5ae05bb67c4b
Luchi-Rach <84988864+Luchi-Rach@users.noreply.github.com>
2022-09-30 23:12:19 +0530

    check box and radio buttton

--- src/modules/UploadingDoc/View/UplaodingDocView.js
+++ src/modules/UploadingDoc/View/UplaodingDocView.js
@@ -1,57 +1,9 @@
 import "../../../../src/styles/App.css";
 import Stepper from "react-stepper-horizontal/lib/Stepper";
-import { useState } from "react";
-import UploadDocument from "../../../component/UploadDocument";
-import CheckResults from "../../../component/CheckResults";
-import DataPoints from "../../../component/DataPoints";
-import DocumentTypes from "../../../component/DocumentTypes";
-const UplaodingDocView = () => {
-  const buttonStyle = {
-    background: "#E0E0E0",
-    width: 200,
-    padding: 16,
-    textAlign: "center",
-    margin: "0 auto",
-    marginTop: 32,
-  };
-  const [activeStep, setActiveStep] = useState(0);
-  const steps = [
-    {
-      title: "Document Types",
-      componenet: <DocumentTypes />,
-      onClick: (e) => {
-        setActiveStep(0);
-      },
-    },
-    {
-      title: "Data Points",
-      componenet: <DataPoints />,
-      onClick: (e) => {
-        setActiveStep(1);
-      },
-    },
-    {
-      title: "Upload Document",
-      componenet: <UploadDocument />,
-      onClick: (e) => {
-        setActiveStep(2);
-      },
-    },
-    {
-      title: "Check Results",
-      componenet: <CheckResults />,
-      onClick: (e) => {
-        setActiveStep(3);
-      },
-    },
-  ];
-  const onClickNext = () => {
-    if (activeStep > 2) {
-      setActiveStep(0);
-    } else {
-      setActiveStep(activeStep + 1);
-    }
-  };
+
+const UplaodingDocView = (props) => {
+  const {steps, activeStep, onClickNext} = props
+  
 
   return (
     <div className="App">
@@ -73,3 +25,12 @@ const UplaodingDocView = () => {
 };
 
 export default UplaodingDocView;
+
+const buttonStyle = {
+  background: "#E0E0E0",
+  width: 200,
+  padding: 16,
+  textAlign: "center",
+  margin: "0 auto",
+  marginTop: 32,
+};
