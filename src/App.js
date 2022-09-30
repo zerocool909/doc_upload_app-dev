f78d1633acd2ea684bc0f5e388fc0d892f388563
nazim1996 <nazimmd1996@gmail.com>
2022-09-30 23:13:53 +0530

    redirect logic added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import "../src/styles/App.css";
 import LoginContainer from "./modules/Login/Container/LoginContainer";
 import UplaodingDocContainer from "./modules/UploadingDoc/Container/UplaodingDocContainer";
 
 function App() {
-  return <LoginContainer></LoginContainer>;
-  // return <UplaodingDocContainer></UplaodingDocContainer>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  console.log(isLoggedIn);
+  return isLoggedIn ? (
+    <UplaodingDocContainer
+      setIsLoggedIn={setIsLoggedIn}
+    ></UplaodingDocContainer>
+  ) : (
+    <LoginContainer setIsLoggedIn={setIsLoggedIn}></LoginContainer>
+  );
 }
 
 export default App;
