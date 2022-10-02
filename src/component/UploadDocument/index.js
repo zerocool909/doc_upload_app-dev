082165444bb8f0a3058703b4a9385ff5c148a969
Luchi-Rach <84988864+Luchi-Rach@users.noreply.github.com>
2022-10-02 16:13:34 +0530

    drag n drop

--- src/component/UploadDocument/index.js
+++ src/component/UploadDocument/index.js
@@ -1,11 +1,36 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
+import {useDropzone} from 'react-dropzone';
+import "./style.css";
 
 const UploadDocument = ({ selectedInfo, setSelectedInfo }) => {
   useEffect(() => {
     console.log('selectedInfo', selectedInfo);;
   }, []);
-  return(
-     <div style={{background : 'red' }}>UploadDocument</div>);
+  const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+  
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
+  return (
+    <div className="document-type-container">
+    <section className="container">
+      <div className="body">
+      <div {...getRootProps({className: 'dropzone'})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      </div>
+      {files.length > 0 ? <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+      : null}
+    </section>
+    </div>
+  );
 };
 
 export default UploadDocument;
